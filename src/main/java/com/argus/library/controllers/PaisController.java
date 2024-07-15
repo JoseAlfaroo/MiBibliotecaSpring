a874e1c6a9a27707a0a74ac8e0d5451365f45bb5
Josè Alfaro <already.5.35@gmail.com>
2024-07-15 17:34:06 -0500

    Agrega método de crear nuevo pais
    - Agrega plantilla de formulario para pais
    - Agrega GET/POST para registro de pais

--- src/main/java/com/argus/library/controllers/PaisController.java
+++ src/main/java/com/argus/library/controllers/PaisController.java
@@ -6,14 +6,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
 import com.argus.library.models.Pais;
 
 @Controller
+@RequestMapping("/paises") 
 public class PaisController {
 	
-	@GetMapping("/paises")
+	@GetMapping(value={"", "/"})
 	private String getAllPaises(Model model) {
 		String uri = "http://localhost:5032/api/Pais";
 		RestTemplate restTemplate = new RestTemplate();
@@ -25,5 +29,24 @@ public class PaisController {
 		
 		return "paises";
 	}
+	
+	
+	@GetMapping("/nuevo")
+	public String crearPais(Model model) {
+	    model.addAttribute("pais", new Pais());
+	    return "formpais";
+	}
+	
+	
+	@PostMapping("/guardar")
+	private String createPais(@ModelAttribute Pais pais) {
+		String uri = "http://localhost:5032/api/Pais";
+		RestTemplate restTemplate = new RestTemplate();
+		
+		restTemplate.postForEntity(uri, pais, Pais.class);
+		
+		return "redirect:/paises";
+	}
+	
 	
 }
