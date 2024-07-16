a903600a3e808a19edc600c62dd81fe9ac1c3dde
Josè Alfaro <already.5.35@gmail.com>
2024-07-16 01:45:06 -0500

    Agrega método actualizar pais
    - Modifica métodos de crear y guardar pais
    - Agrega método para editar un pais
    - Agrega texto condicional en plantilla "formpais"

--- src/main/java/com/argus/library/controllers/PaisController.java
+++ src/main/java/com/argus/library/controllers/PaisController.java
@@ -7,46 +7,94 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.argus.library.models.Pais;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/paises") 
 public class PaisController {
-	
-	@GetMapping(value={"", "/"})
-	private String getAllPaises(Model model) {
-		String uri = "http://localhost:5032/api/Pais";
-		RestTemplate restTemplate = new RestTemplate();
-		
-		ResponseEntity<Pais[]> response = restTemplate.getForEntity(uri, Pais[].class);
-		
-		List<Pais> paises = Arrays.asList(response.getBody());
-		model.addAttribute("paises", paises);
-		
-		return "paises";
-	}
-	
-	
-	@GetMapping("/nuevo")
-	public String crearPais(Model model) {
-	    model.addAttribute("pais", new Pais());
-	    return "formpais";
-	}
-	
-	
-	@PostMapping("/guardar")
-	private String createPais(@ModelAttribute Pais pais) {
-		String uri = "http://localhost:5032/api/Pais";
-		RestTemplate restTemplate = new RestTemplate();
-		
-		restTemplate.postForEntity(uri, pais, Pais.class);
-		
-		return "redirect:/paises";
-	}
-	
-	
+
+    @GetMapping(value = {"", "/"})
+    private String getAllPaises(Model model) {
+        String uri = "http://localhost:5032/api/Pais";
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<Pais[]> response = restTemplate.getForEntity(uri, Pais[].class);
+        List<Pais> paises = Arrays.asList(response.getBody());
+        model.addAttribute("paises", paises);
+
+        return "paises";
+    }
+
+    
+    @GetMapping("/nuevo")
+    public String crearPais(Model model, HttpSession session) {
+        model.addAttribute("pais", new Pais());
+        model.addAttribute("crear", true);
+        session.setAttribute("crear", true);
+        
+        return "formpais";
+    }
+
+    
+    @GetMapping("/editar/{id}")
+    public String editarPais(@PathVariable int id, Model model, HttpSession session) {
+        String uri = "http://localhost:5032/api/Pais/" + id;
+        RestTemplate restTemplate = new RestTemplate();
+
+        try {
+            ResponseEntity<Pais> response = restTemplate.getForEntity(uri, Pais.class);
+            Pais pais = response.getBody();
+            model.addAttribute("pais", pais);
+            
+            model.addAttribute("crear", false);
+            session.setAttribute("PaisID", pais.getPaisID());
+            session.setAttribute("crear", false);
+            
+            return "formpais";
+        } catch (HttpClientErrorException e) {
+            System.out.println("Codigo de error: " + e.getStatusCode());
+            return "redirect:/paises";
+        }
+    }
+
+    
+    @PostMapping("/guardar")
+    private String savePais(@ModelAttribute Pais pais, Model model, HttpSession session) {
+        RestTemplate restTemplate = new RestTemplate();
+        String uri = "http://localhost:5032/api/Pais";
+        
+        if(session.getAttribute("crear") == null) {
+        	return "redirect:/paises";
+        };
+        
+        Boolean crear = (Boolean) session.getAttribute("crear");
+
+        if (crear) {
+        	
+            restTemplate.postForEntity(uri, pais, Pais.class);
+            session.removeAttribute("crear");
+            
+            return "redirect:/paises";
+        } 
+        else {
+        	int PaisID = (int) session.getAttribute("PaisID");
+
+            uri = uri + "/" + PaisID;
+            restTemplate.put(uri, pais);
+            session.removeAttribute("crear");
+        	
+            return "redirect:/paises";
+        }
+
+    }
+    
+    
 }
