1d0f3eda4f86ecf38bc790cab2fad81484fe2f68
Josè Alfaro <already.5.35@gmail.com>
2024-07-18 01:02:28 -0500

    Agrega 'Editar Autor'
    - Agrega método 'editarAutor' en AutorController
    - Modifica plantillas para manejo de referencias nulas

--- src/main/java/com/argus/library/controllers/AutorController.java
+++ src/main/java/com/argus/library/controllers/AutorController.java
@@ -8,8 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.argus.library.models.Autor;
@@ -53,6 +55,34 @@ public class AutorController {
         session.setAttribute("crear", true);
         
         return "formautor";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String editarAutor(@PathVariable int id, Model model, HttpSession session) {
+        String uri = "http://localhost:5032/api/Autor/" + id;
+        String uriPaises = "http://localhost:5032/api/Pais";
+        RestTemplate restTemplate = new RestTemplate();      
+        
+        try {
+            ResponseEntity<Autor> response = restTemplate.getForEntity(uri, Autor.class);
+            Autor autor = response.getBody();
+    		
+            ResponseEntity<Pais[]> responsePaises = restTemplate.getForEntity(uriPaises, Pais[].class);
+            List<Pais> paises = Arrays.asList(responsePaises.getBody());
+            
+            model.addAttribute("paises", paises);
+            model.addAttribute("autor", autor);
+            model.addAttribute("crear", false);
+            
+            session.setAttribute("AutorID", autor.getAutorID());
+            session.setAttribute("crear", false);
+            
+            return "formautor";
+        }
+        catch (HttpClientErrorException e) {
+            System.out.println("Codigo de error: " + e.getStatusCode());
+            return "redirect:/autores";
+        }
 	}
 	
 	
