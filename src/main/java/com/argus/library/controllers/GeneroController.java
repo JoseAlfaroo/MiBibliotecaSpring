9ae159dbc51a9974b5e402e2f9378f50a3c2d4b0
Josè Alfaro <already.5.35@gmail.com>
2024-07-18 22:13:28 -0500

    Implementa métodos de crear-editar-eliminar genero
    - Agrega la plantilla "formgenero"
    - Actualiza GeneroController con métodos crear-editar-eliminar

--- src/main/java/com/argus/library/controllers/GeneroController.java
+++ src/main/java/com/argus/library/controllers/GeneroController.java
@@ -7,10 +7,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.argus.library.models.Genero;
+import com.argus.library.models.Pais;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/generos") 
@@ -28,6 +35,87 @@ public class GeneroController {
     	return "generos";
     }
     
+    
+    @GetMapping("/nuevo")
+    public String crearGenero(Model model, HttpSession session) {
+        model.addAttribute("genero", new Genero());
+        model.addAttribute("crear", true);
+        session.setAttribute("crear", true);
+        
+        return "formgenero";
+    }
+    
+    
+    @GetMapping("/editar/{id}")
+    public String editarGenero(@PathVariable int id, Model model, HttpSession session) {
+        String uri = "http://localhost:5032/api/Genero/" + id;
+        RestTemplate restTemplate = new RestTemplate();
+
+        try {
+            ResponseEntity<Genero> response = restTemplate.getForEntity(uri, Genero.class);
+            Genero genero = response.getBody();
+            model.addAttribute("genero", genero);
+            
+            model.addAttribute("crear", false);
+            session.setAttribute("GeneroID", genero.getGeneroID());
+            session.setAttribute("crear", false);
+            
+            return "formgenero";
+        } 
+        catch (HttpClientErrorException e) {
+            System.out.println("Codigo de error: " + e.getStatusCode());
+            return "redirect:/generos";
+        }
+    }
+    
+    
+    
+    @PostMapping("/guardar")
+    private String saveGenero(@ModelAttribute Genero genero, Model model, HttpSession session) {
+        RestTemplate restTemplate = new RestTemplate();
+        String uri = "http://localhost:5032/api/Genero";
+        
+        if(session.getAttribute("crear") == null) {
+            return "redirect:/generos";
+        };
+        
+        Boolean crear = (Boolean) session.getAttribute("crear");
+
+        if (crear) {
+        	
+            restTemplate.postForEntity(uri, genero, Genero.class);
+            session.removeAttribute("crear");
+            
+            return "redirect:/generos";
+        } 
+        else {
+        	int GeneroID = (int) session.getAttribute("GeneroID");
+
+            uri = uri + "/" + GeneroID;
+            restTemplate.put(uri, genero);
+            session.removeAttribute("crear");
+        	
+            return "redirect:/generos";
+        }
+
+    }
+    
+    
+    @PostMapping("/eliminar/{id}")
+    private String deleteGenero(@PathVariable int id) {
+        String uri = "http://localhost:5032/api/Genero/" + id;
+        RestTemplate restTemplate = new RestTemplate();
+        
+        try {
+        	restTemplate.delete(uri);
+        }
+        catch (HttpClientErrorException e) {
+            System.out.println("Codigo de error: " + e.getStatusCode());
+            return "redirect:/generos";
+        }
+    	
+        return "redirect:/generos";
+    }
     
     
 }
